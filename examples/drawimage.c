779691a17c3082ea1ed2f8c971e29ec96d9c1d44
rprata <renancprata@gmail.com>
2017-03-26 13:36:11 -0300

    adjusting examples for LCD screen (3.5'')

--- examples/drawimage.c
+++ examples/drawimage.c
@@ -17,11 +17,13 @@ int main (int argc, char ** argv)
 		desc.buffer = DOUBLE; 
 		if(ibutterfree_create_surface(&surface, &desc) == IBUTTERFREE_OK)
 		{
+			int xres, yres;
+			ibutterfree_get_resolution(&surface, &xres, &yres);
 			ibutterfree_clear_surface(&surface, (int32_t) 0x000000FF);
-			ibutterfree_draw_image(&surface, "../examples/resources/images/IButterFree.bmp", 200, 200, 50, 50, IBUTTERFREE_IMAGE_TYPE_BMP);
-			ibutterfree_draw_image(&surface, "../examples/resources/images/IButterFree.ppm", 400, 400, 150, 200, IBUTTERFREE_IMAGE_TYPE_PPM);
-			ibutterfree_draw_image(&surface, "../examples/resources/images/IButterFree.png", 200, 400, 200, 150, IBUTTERFREE_IMAGE_TYPE_PNG);
-			ibutterfree_draw_image(&surface, "../examples/resources/images/IButterFree.png", 400, 200, -1, -1, IBUTTERFREE_IMAGE_TYPE_PNG);
+			ibutterfree_draw_image(&surface, "../examples/resources/images/IButterFree.bmp", xres/7, yres/4, xres/7, xres/7, IBUTTERFREE_IMAGE_TYPE_BMP);
+			ibutterfree_draw_image(&surface, "../examples/resources/images/IButterFree.ppm", xres/4, yres/2, xres/7, xres/7, IBUTTERFREE_IMAGE_TYPE_PPM);
+			ibutterfree_draw_image(&surface, "../examples/resources/images/IButterFree.png", xres/7, yres/2, xres/7, xres/7, IBUTTERFREE_IMAGE_TYPE_PNG);
+			ibutterfree_draw_image(&surface, "../examples/resources/images/IButterFree.png", xres/4, yres/4, xres/7, xres/7, IBUTTERFREE_IMAGE_TYPE_PNG);
 			ibutterfree_flip(&surface);
 			ibutterfree_destroy_surface(&surface);
 		}		
