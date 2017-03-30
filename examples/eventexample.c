5b7f8bbb82ebba89188fd8a66388a5644a7ce74f
rprata <renancprata@gmail.com>
2017-03-30 01:58:31 -0300

    adding new feature for event example

--- examples/eventexample.c
+++ examples/eventexample.c
@@ -19,14 +19,29 @@ int main (int argc, char ** argv)
 		{
 			int xres, yres;
 			ibutterfree_get_resolution(&surface, &xres, &yres);
+			int r = xres/4;
 			ibutterfree_clear_surface(&surface, (int32_t) 0x000000FF);
 			ibutterfree_event_init(&surface);
-
+			ibutterfree_set_color(&surface, (int32_t)0x00FF00FF);
+			ibutterfree_fill_circle(&surface, xres/2, yres/2, r);
+			ibutterfree_flip(&surface);
 			IButterFreeTouchStruct bfts;
 			while(1)
 			{
 				ibutterfree_event_read(&bfts);
 				IBUTTERFREE_LOG_TRACK("x: %d y: %d  pressure: %d", bfts.x_abs, bfts.y_abs, bfts.pressure);
+				ibutterfree_clear_surface(&surface, (int32_t) 0x000000FF);
+				if ((bfts.x_abs - xres/2)*(bfts.x_abs - xres/2) + 
+					(bfts.y_abs - yres/2)*(bfts.y_abs - yres/2) <=  (r - xres/2)*(r - yres/2))
+				{
+					ibutterfree_set_color(&surface, (int32_t)0xFF0000FF);
+				}
+				else
+				{
+					ibutterfree_set_color(&surface, (int32_t)0x00FF00FF);
+				}
+				ibutterfree_fill_circle(&surface, xres/2, yres/2, r);
+				ibutterfree_flip(&surface);
 			}
 			ibutterfree_event_destroy();
 			ibutterfree_destroy_surface(&surface);
